0dcf5ca9fbc46682105a28e459a15b0d68dd3158
canto9112 <cantoweb112@gmail.com>
2021-11-17 21:41:09 +0300

    Обновил статику

--- self_storage/settings.py
+++ self_storage/settings.py
@@ -126,12 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
+STATIC_ROOT = env.str("STATIC_ROOT", default=os.path.join(BASE_DIR, "collected_static"))
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = env.str("MEDIA_ROOT", default=os.path.join(BASE_DIR, "media"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
