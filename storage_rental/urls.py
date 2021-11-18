1670f8aa5b3678973d79a24326c2163bbcd0827d
canto9112 <cantoweb112@gmail.com>
2021-11-18 09:26:24 +0300

    Убрал из карты всплывашку. Оставил только Адрес

--- storage_rental/urls.py
+++ storage_rental/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from . import views
-from storage_rental.views import get_place_view
 
 
 urlpatterns = [
@@ -12,6 +11,5 @@ urlpatterns = [
         views.StoringOrderView.as_view(),
         name='deposit_items',
     ),
-    path('<int:place_id>/', get_place_view, name='place_view'),
     path('application_form', views.application_form, name='application_form'),
 ]
