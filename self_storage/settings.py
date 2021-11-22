ae623d1f04ad231fd16361ef9f52af548d5759ac
balancy <balancy@gmail.com>
2021-11-22 14:38:55 +0100

    Make repo ready for deploy

--- self_storage/settings.py
+++ self_storage/settings.py
@@ -29,7 +29,7 @@ env.read_env()
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
@@ -126,11 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
-STATIC_ROOT = env.str("STATIC_ROOT", default=os.path.join(BASE_DIR, "collected_static"))
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = env.str("MEDIA_ROOT", default=os.path.join(BASE_DIR, "media"))
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- self_storage/urls.py
+++ self_storage/urls.py
@@ -21,9 +21,4 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('storage_rental.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
